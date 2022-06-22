76769d3e82bd741fd3daf4e07a844f7c09175b5b
Roman Vladimirov <trueromanus@gmail.com>
2022-06-22 21:34:22 +0300

    + Added documentation scaffold

--- src/components/controls/DocumentationPreview.vue
+++ src/components/controls/DocumentationPreview.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="documentation-container">
-        <img src="/assets/icons/question.svg" width="50" height="50" />
+        <div class="documentation-content">
+            <div class="documentation-header">
+                Documentation
+            </div>
+            <div>
+                <img src="/assets/icons/question.svg" width="50" height="50" />
+            </div>
+            <div class="documentation-columns" v-if="items">
+                <div class="documentation-right-align">
+                    <div v-for="(leftitem, index) in items.leftColumn" :key="index">
+                        <span>{{ leftitem.title }} </span>
+                    </div>
+                </div>
+                <div class="documentation-left-align">
+                    <div v-for="(rightitem, index) in items.rightColumn" :key="index">
+                        <span>{{ rightitem.title }} </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,7 +27,7 @@
 export default {
     name: `DocumentationPreview`,
     props: {
-        documentation: {
+        items: {
             type: Object,
             required: true
         }
@@ -18,9 +37,46 @@ export default {
 
 <style>
 .documentation-container {
-    width: 90%;    
+    width: 90%;
     border-collapse: collapse;
-    border: 1px solid lightgray;
     font-family: 'Open Sans', sans-serif;
+    display: flex;
+    justify-content: center;
+}
+.documentation-content {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    max-width: 750px;
+    width: 750px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+}
+.documentation-header {
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 40px;
+    margin-bottom: 18px;
+}
+.documentation-columns {
+    display: flex;
+    flex-direction: row;
+}
+.documentation-columns > div {
+    margin-top: 10px;
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.documentation-right-align {
+    align-items: flex-end;
+    margin-right: 10px;
+}
+.documentation-left-align {
+    align-items: flex-start;
+    margin-left: 10px;
 }
 </style>
