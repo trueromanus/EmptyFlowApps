c108df00b0635c67a6b89f8614c08405d16aa5dd
Roman Vladimirov <trueromanus@gmail.com>
2022-06-24 22:26:00 +0300

    + Added documentation page

---
+++ src/components/pages/DocumentationPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="documentation-content">
+            <div class="documentation-header">
+                {{ documentationPage.title }}
+            </div>
+            <div>
+                <img src="/assets/icons/question.svg" width="50" height="50" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: `DocumentationPage`,
+    data() {
+        return {
+            documentationPage: {}
+        }
+    }
+}
+</script>
+
+<style>
+ 
+</style>
